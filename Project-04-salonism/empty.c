1cf0815e424c1de0bb3a266c68ac1ab985ab69b9
lmaosaloni <73757622+lmaosaloni@users.noreply.github.com>
2024-03-06 21:06:52 +0600

    Add files via upload
    Porject 4 - Salonism

---
+++ Project-04-salonism/empty.c
@@ -0,0 +1,9 @@
+// this program is empty
+//
+// it only contains these lines of comments
+// no simpleC code is present
+//
+// This file will show success for the initial starter code without changes
+// HOWEVER, that is not a valid simpleC program
+//
+// This program should fail when the parser is working
---
+++ Project-04-salonism/tokentester.c
@@ -0,0 +1,12 @@
+//
+// another simple C test program
+//
+void main()
+{
+    int "string with identifier in it" ;
+    int x     // syntax error: expecting ;, but found identifier:x
+
+    x = 0;
+    cout << x;
+    cout << endl;
+}
